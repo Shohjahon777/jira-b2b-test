16266f10ab303fa924180dfc33e07f9e4ea7827c
Shohjahon <razzoqovshohjahon55@gmail.com>
2025-02-08 13:14:54 +0500

    Final changes

--- client/src/components/workspace/project/edit-project-dialog.tsx
+++ client/src/components/workspace/project/edit-project-dialog.tsx
@@ -1,8 +1,9 @@
 import { Edit3 } from "lucide-react";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {Dialog, DialogContent, DialogTitle, DialogTrigger} from "@/components/ui/dialog";
 import EditProjectForm from "./edit-project-form";
 import { ProjectType } from "@/types/api.type";
 import { useState } from "react";
+import Logo from "@/components/logo";
 
 const EditProjectDialog = (props: { project?: ProjectType }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,6 +20,14 @@ const EditProjectDialog = (props: { project?: ProjectType }) => {
           </button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-lg border-0">
+          <DialogTitle>
+            <div className="flex h-[30px] items-center justify-center w-full px-1">
+              <Logo />
+              <div className="hidden md:flex ml-2 items-center gap-2 self-center font-medium">
+                Team Sync.
+              </div>
+            </div>
+          </DialogTitle>
           <EditProjectForm project={props.project} onClose={onClose} />
         </DialogContent>
       </Dialog>
---
+++ client/src/hooks/use-create-task-dialog.ts
@@ -0,0 +1,17 @@
+import { parseAsBoolean, useQueryState } from "nuqs";
+
+const useCreateTaskDialog = () => {
+    const [open, setOpen] = useQueryState(
+        "new-task",
+        parseAsBoolean.withDefault(false)
+    );
+    const onOpen = () => setOpen(true);
+    const onClose = () => setOpen(false);
+    return {
+        open,
+        onOpen,
+        onClose,
+    };
+};
+
+export default useCreateTaskDialog;
